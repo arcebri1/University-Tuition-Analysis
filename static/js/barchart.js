1defd0ade600a68a24595643257812ab6636b856
Lorena Arce <you@example.com>
2020-12-30 02:18:52 -0800

    have some bar charts, still working on the rest

--- static/js/barchart.js
+++ static/js/barchart.js
@@ -1,52 +1,5 @@
-// getData()
-
-// async function getData() {
-//     const response = await fetch('datasets/mergednoindex_data.csv');
-//     const data = await response.text();
-//     console.log(data)
-
-//     const table = data.split('\n').slice(1);
-//     table.forEach(row => {
-//         const columns = row.split(' , ');
-//         // const type = columns[1];
-//         // const temp = columns[1];
-//         console.log(columns);
-//     })
-//     // console.log(rows);
-// }
 
 d3.csv("datasets/mergednoindex_data.csv").then(function (schoolData) {
-
-
-    // let types = schoolData.map(function (d) {
-    //     return d.type
-    // })
-
-    // console.log(types)
-
-    // let typesPrivate = schoolData.map(function (d) {
-    //     return d.type === 'Private'
-    // })
-
-    // console.log(typesPrivate)
-
-    // let typesColor = schoolData.map(function (d) {
-    //     return d.type === 'Private' ? '#F15F36' : '#19A0AA';
-    // })
-
-    // console.log(typesColor)
-
-    // let tuition = schoolData.map(function (d) {
-    //     return +d.in_state_tuition
-    // })
-
-    // console.log(tuition)
-
-    // let schoolName = schoolData.map(function (d) {
-    //     return d.name_x
-    // })
-
-    // console.log(schoolName)
 
     let inStateTuition = 'in_state_tuition';
 
@@ -114,62 +67,12 @@ d3.csv("datasets/mergednoindex_data.csv").then(function (schoolData) {
 
     console.log(totalPublicTuition)
 
-    
+    let avgPrivateTuition = Math.round(totalPrivateTuition / totalData)
+    console.log(avgPrivateTuition)
 
-    // // chart options
-    // const options = {
-    //     chart: {
-    //         height: 450,
-    //         width: '100%',
-    //         type: 'bar',
-    //         background: '#f4f4f4',
-    //         foreColor: '#333'
-    //     },
-    //     series: [{
-    //         name: 'Tuition',
-    //         data: [12500, 23654]
-    //     }],
-    //     xaxis: {
-    //         categories: [
-    //             'Private',
-    //             'Public']
-    //     },
-    //     plotOptions: {
-    //         bar: {
-    //             horizontal: false
-    //         }
-    //     },
-    //     fill: {
-    //         colors: ['#f44336']
-    //     },
-    //     dataLabels: {
-    //         enabled: false
-    //     },
-    //     title: {
-    //         text: 'Top 300 Universities/Colleges in US',
-    //         align: 'center',
-    //         margin: 20,
-    //         offsetY: 20,
-    //         style: {
-    //             fontSize: '25px'
-    //         }
-    //     }
-    // }
+    let avgPublicTuition = Math.round(totalPublicTuition / totalData)
+    console.log(avgPublicTuition)
 
-    // // init chart
-    // const chart = new ApexCharts(document.querySelector('#chart'), options);
-
-    // // render chart
-    // chart.render();
-
-    // //create event method
-    // document.querySelector('button').addEventListener('click', () => chart.updateOptions({
-    //     plotOptions: {
-    //         bar: {
-    //             horizontal: true
-    //         }
-    //     }
-    // }))
 
     //chart.js
 
@@ -184,7 +87,7 @@ d3.csv("datasets/mergednoindex_data.csv").then(function (schoolData) {
             datasets: [{
                 label: 'Private',
                 // type: 'bar',
-                backgroundColor: 'rgb(0,99,132',
+                backgroundColor: '#00008B',
                 borderColor: 'black',
                 borderWidth: 1,
                 hoverBackgroundColor: 'green',
@@ -194,7 +97,7 @@ d3.csv("datasets/mergednoindex_data.csv").then(function (schoolData) {
             {
                 label: 'Public',
                 // type: 'bar',
-                // backgroundColor: 'rgb(2, 99, 132)',
+                backgroundColor: '#B8860B',
                 borderColor: 'black',
                 borderWidth: 1,
                 hoverBackgroundColor: 'red',
@@ -244,21 +147,21 @@ d3.csv("datasets/mergednoindex_data.csv").then(function (schoolData) {
             datasets: [{
                 label: 'Private',
                 // type: 'bar',
-                backgroundColor: 'rgb(0,99,132',
+                backgroundColor: '#00FFFF',
                 borderColor: 'black',
                 borderWidth: 1,
                 hoverBackgroundColor: 'green',
-                data: [privateSum, totalData],
+                data: [avgPrivateTuition],
                 order: 1
             },
             {
                 label: 'Public',
                 // type: 'bar',
-                // backgroundColor: 'rgb(2, 99, 132)',
+                backgroundColor: '#7FFFD4',
                 borderColor: 'black',
                 borderWidth: 1,
                 hoverBackgroundColor: 'red',
-                data: [publicSum],
+                data: [avgPublicTuition],
                 order: 2
             },
             ]
@@ -272,7 +175,7 @@ d3.csv("datasets/mergednoindex_data.csv").then(function (schoolData) {
             },
             title: {
                 display: true,
-                text: 'Avg Tuition for Public & Private Schools',
+                text: 'Avg Cost of In-State Tuition for Public & Private Schools',
                 fontSize: 15
             },
             legend: {
@@ -294,5 +197,141 @@ d3.csv("datasets/mergednoindex_data.csv").then(function (schoolData) {
     });
 
 
+
+    let totalPriTuition = privateSchools.map(d => d.in_state_total)
+    console.log(totalPriTuition)
+
+    //syntax: array.reduce(function(total, currentValue, currentIndex, arr), initialValue)
+    let totalCoursePrivateTuition = totalPriTuition.reduce(
+        function (t, s) {
+            return parseInt(t) + parseInt(s);
+        }
+    );
+
+    console.log(totalCoursePrivateTuition)
+
+    let totalPubTuition = publicSchools.map(d => d.in_state_total)
+    console.log(totalPubTuition)
+
+    //syntax: array.reduce(function(total, currentValue, currentIndex, arr), initialValue)
+    let totalCoursePublicTuition = totalPubTuition.reduce(
+        function (t, s) {
+            return parseInt(t) + parseInt(s);
+        }
+    );
+
+    console.log(totalCoursePublicTuition)
+
+    let totalAvgPrivateTuition = Math.round(totalCoursePrivateTuition / totalData)
+    console.log(totalAvgPrivateTuition)
+
+    let totalAvgPublicTuition = Math.round(totalCoursePublicTuition / totalData)
+    console.log(totalAvgPublicTuition)
+
+
+    var ctx = document.getElementById('myChart3').getContext('2d');
+    var chartjs = new Chart(ctx, {
+        // The type of chart we want to create
+        type: 'bar',
+
+        // The data for our dataset
+        data: {
+            labels: ['Private & Public'],
+            datasets: [{
+                label: 'Private',
+                // type: 'bar',
+                backgroundColor: '#2F4F4F',
+                borderColor: 'black',
+                borderWidth: 1,
+                hoverBackgroundColor: 'green',
+                data: [totalAvgPrivateTuition],
+                order: 1
+            },
+            {
+                label: 'Public',
+                // type: 'bar',
+                backgroundColor: '#E9967A',
+                borderColor: 'black',
+                borderWidth: 1,
+                hoverBackgroundColor: 'red',
+                data: [totalAvgPublicTuition],
+                order: 2
+            },
+            ]
+        },
+
+
+        // Configuration options go here
+        options: {
+            tooltips: {
+                mode: 'index',
+            },
+            title: {
+                display: true,
+                text: 'Avg Total Cost of In-State Tuition for Public & Private Schools',
+                fontSize: 15
+            },
+            legend: {
+                position: 'right',
+                labels: {
+                    fontColor: '#000'
+                }
+            },
+            layout: {
+                padding: {
+                    left: 10,
+                    right: 10,
+                    bottom: 0,
+                    top: 0
+                }
+            },
+
+        }
+    });
+
+    function select2Yr(school) {
+        return school.degree_length === '2 Year'
+    }
+
+    // console.log(select2Yr)
+
+        let twoYrSchools = schoolData.filter(select2Yr)
+        console.log(twoYrSchools)
+
+        let twoYrTuition = twoYrSchools.map(d => d.in_state_tuition)
+        console.log(twoYrTuition)
+
+    //     //syntax: array.reduce(function(total, currentValue, currentIndex, arr), initialValue)
+    //     let totalPrivateTuition = privateTuition.reduce(
+    //         function (t, s) {
+    //             return parseInt(t) + parseInt(s);
+    //         }
+    //     );
+
+    //     console.log(totalPrivateTuition)
+
+    //     function selectPublic(school) {
+    //         return school.type === 'Public'
+    //     }
+    //     let publicSchools = schoolData.filter(selectPublic)
+    //     console.log(publicSchools)
+
+    //     let publicTuition = publicSchools.map(d => d.in_state_tuition)
+    //     console.log(publicTuition)
+
+    //     //syntax: array.reduce(function(total, currentValue, currentIndex, arr), initialValue)
+    //     let totalPublicTuition = publicTuition.reduce(
+    //         function (t, s) {
+    //             return parseInt(t) + parseInt(s);
+    //         }
+    //     );
+
+    //     console.log(totalPublicTuition)
+
+    //     let avgPrivateTuition = Math.round(totalPrivateTuition / totalData)
+    //     console.log(avgPrivateTuition)
+
+    //     let avgPublicTuition = Math.round(totalPublicTuition / totalData)
+    //     console.log(avgPublicTuition)
 
 })
