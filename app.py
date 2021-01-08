a1ec8d53b7900abc3da1953625c38fcc5a1d4bea
Michelle Sullivan <michelleecochran@gmail.com>
2021-01-08 15:32:32 -0800

    correcting nav bar routes on bubble page

--- app.py
+++ app.py
@@ -16,11 +16,11 @@ app = Flask(__name__)
 def home():
     return render_template("base.html")
 
-@app.route("/scatterplot")
+@app.route("/scatter")
 def scatter():
     return render_template("Salary.html")
 
-@app.route("/bubblechart")
+@app.route("/bubble")
 def bubble():
     return render_template("bubble.html")
 
@@ -29,10 +29,20 @@ def bullet():
     return render_template("bullet.html")
 
 @app.route("/merged_data")
-def read_sql():
+def read_merged_sql():
     conn = psycopg2.connect(dbname="project-2", user="postgres", password="postgres", host="127.0.0.1", port="5432")
 
     dataframe = pd.read_sql("SELECT * FROM merged_data", conn)
+
+    json_data = dataframe.to_json(orient="records")
+
+    return json_data
+
+@app.route("/tuition_vs_salary")
+def read_salary_sql():
+    conn = psycopg2.connect(dbname="project-2", user="postgres", password="postgres", host="127.0.0.1", port="5432")
+
+    dataframe = pd.read_sql("SELECT * FROM tuition_vs_salary", conn)
 
     json_data = dataframe.to_json(orient="records")
 
